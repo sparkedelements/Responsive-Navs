c9232c6645cff7a575cd97d5044c8326342c35c2
SE <DansComputer@dejans-imac.attlocal.net>
2015-01-03 01:58:51 -0500

    JQuery Update
    Made to activate with css condition upon media query.

--- js/nav.js
+++ js/nav.js
@@ -2,19 +2,30 @@
 
 
 
-$(document).ready(function(){
-	
-	$(".pull").click(function(){
-	$("nav ul").toggle();
-	});
-	
-	$(".toggle1").click(function(){
-	$(".toggle1 ul").toggle();
-	});		
-	
-	$(".toggle2").click(function(){
-	$(".toggle2 ul").toggle();
-	});
 
 
+
+$(window).resize(function(){	
+	if ($(".navmenu").css("display") == "block" ){
+	
+			$(document).ready(function(){
+			
+				$(".pull").click(function(){
+				$("nav ul").toggle();
+				});
+				
+				$(".toggle1").click(function(){
+				$(".toggle1 ul").toggle();
+				});		
+				
+				$(".toggle2").click(function(){
+				$(".toggle2 ul").toggle();
+				});
+				
+			});	
+	}	
 });
+
+
+
+
