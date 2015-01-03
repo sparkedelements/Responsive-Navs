3174801873dbd5ed30dd7646505130f06c4ea4c4
SE <DansComputer@dejans-imac.attlocal.net>
2015-01-03 13:47:21 -0500

    Jquery Customization
    Working on bug fixes. JQuery does not work on browser resize to down.
    Also, if resize from large to small then back to large nav bar
    disappears.

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+$(window).resize(function() {
+
+
+$(document).ready(function() {
+    function doneResizing() {
+        if(Modernizr.mq('screen and (min-width:768px)')) {
+            
+	
+			
+        }
+        else if(Modernizr.mq('screen and (max-width:767px)')) {
+            
+			$(".pull").click(function(){
+				$("nav ul").toggle();
+				});
+				
+				$(".toggle1").click(function(){
+				$(".toggle1 ul").toggle();
+				});		
+				
+				$(".toggle2").click(function(){
+				$(".toggle2 ul").toggle();
+				});
+			
+        }
+    }
+
+    var id;
+    $(window).resize(function() {
+        clearTimeout(id);
+        id = setTimeout(doneResizing, 0);
+    });
+
+    doneResizing();
+});
+
+}).trigger("resize");
+
+
+
+
--- js/nav.js
+++ js/nav.js
@@ -5,27 +5,5 @@
 
 
 
-$(window).resize(function(){	
-	if ($(".navmenu").css("display") == "block" ){
-	
-			$(document).ready(function(){
-			
-				$(".pull").click(function(){
-				$("nav ul").toggle();
-				});
-				
-				$(".toggle1").click(function(){
-				$(".toggle1 ul").toggle();
-				});		
-				
-				$(".toggle2").click(function(){
-				$(".toggle2 ul").toggle();
-				});
-				
-			});	
-	}	
-});
-
-
 
 
