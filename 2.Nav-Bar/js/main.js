3430914b4eced6e4a276c0e4c8b1397f33a1ae1b
SE <DansComputer@dejans-imac.attlocal.net>
2015-01-05 00:20:59 -0500

    Working on JQuery
    Getting errors upon browser resize.

--- 2.Nav-Bar/js/main.js
+++ 2.Nav-Bar/js/main.js
@@ -1,1 +1,31 @@
+$(document).ready(function() {
+    function doneResizing() {
+        if(Modernizr.mq('screen and (min-width:768px)')) {
+            
 	
+			
+        }
+        else if(Modernizr.mq('screen and (max-width:767px)')) {
+            
+			$(".pull").click(function(){
+				$(".mobilemenu").toggle();
+				});
+				
+			$(".click1").click(function(){
+				$(".click1 ul").toggle();
+				});		
+			
+        }
+    }
+
+    var id;
+    $(window).resize(function() {
+        clearTimeout(id);
+        id = setTimeout(doneResizing, 0);
+    });
+
+    doneResizing();
+});
+
+
+
